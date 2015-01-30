fb297c18dcd0f3220eceafbacfab2b2c5d0296df
JenilynnB <jenniferlbrandes@gmail.com>
2015-01-29 17:30:26 -0800

    Homepage display changes

--- wp-content/themes/ammo/framework/widgets/recent_posts_widget.php
+++ wp-content/themes/ammo/framework/widgets/recent_posts_widget.php
@@ -220,3 +220,99 @@ class Carnaby_RecentPostsWidget extends WP_Widget {
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("Carnaby_RecentPostsWidget");'));
+
+class Carnaby_RecentPostsStaticWidget extends WP_Widget {
+
+    function Carnaby_RecentPostsStaticWidget() {
+        $widget_ops = array('classname' => 'carnaby-recent-posts-static-widget', 'description' => 'Recent posts');
+        parent::WP_Widget(false, 'Carnaby Recent Posts Static', $widget_ops);
+    }
+
+    function widget($args, $instance) {
+        global $post;
+        extract(array_merge(array(
+                    'title' => '',
+                    'number_posts' => 3,
+                    'thumb' => 'thumb',
+                        ), $instance));
+
+        $q['posts_per_page'] = $number_posts;
+        $q['ignore_sticky_posts'] = 1;
+
+        query_posts($q);
+
+        if (isset($before_widget))
+            echo $before_widget;
+
+        
+        if ($title != '')
+            echo $args['before_title'] . $title . $args['after_title'];
+
+        echo '<div class="widget widget-posts">';
+        while (have_posts()) : the_post();
+            echo '<div class="widget-post col-sm-12 col-xs-12 col-md-4">';
+            $comments = wp_count_comments($post->ID);
+            
+            
+            if(has_post_thumbnail()) {
+                $thumbnail_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'medium');
+                $thumbnail_url = $thumbnail_url[0];
+                
+                //echo '<img src="'.$thumbnail_url.'" width="300" height="200">';
+                //the_post_thumbnail(array(300,200));
+            } else {
+                echo '<span class="entry-format"></span>';
+            }
+            echo '<div class="widget-thumb-large" style="background-image: url('.$thumbnail_url.')">';
+            
+            echo '</div>';
+            
+            echo '<div class="widget-posts-title">';
+            echo '<a href="'.get_permalink().'">'.get_the_title().'</a>';
+            /*echo '<ul class="list-inline">
+                    <li><a href="#" title=""><i class="fa fa-comments"></i> '.$comments->total_comments.'</a></li>
+                    <li>'. get_post_like(get_the_ID()) .'</li>
+                  </ul>';*/
+            echo '<span class="link-icon"><i class="fa fa-arrow-right"></i></span>';
+            echo '</div></div>';
+        endwhile;
+        echo '</div>';
+        
+
+        wp_reset_query();
+    }
+
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        /* Strip tags (if needed) and update the widget settings. */
+        $instance['title'] = sanitize_text_field($new_instance['title']);
+        $instance['number_posts'] = sanitize_text_field($new_instance['number_posts']);
+        $instance['thumb'] = sanitize_text_field($new_instance['thumb']);
+
+        return $instance;
+    }
+
+    function form($instance) {
+
+        //Output admin widget options form
+        extract(shortcode_atts(array(
+                    'title' => '',
+                    'thumb' => 'thumb',
+                    'number_posts' => 5,
+                        ), $instance));
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e("Title:", "themeton"); ?></label>
+            <input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>"  />
+        </p>
+        
+        <p>
+            <label for="<?php echo $this->get_field_id('number_posts'); ?>">Number of posts to show:</label>
+            <input type="text" id="<?php echo $this->get_field_id('number_posts'); ?>" name="<?php echo $this->get_field_name('number_posts'); ?>" value="<?php echo $number_posts; ?>" size="3" />
+        </p>
+        <?php
+    }
+
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Carnaby_RecentPostsStaticWidget");'));
--- wp-content/themes/ammo/front-page.php
+++ wp-content/themes/ammo/front-page.php
@@ -28,8 +28,118 @@ get_header();
                     </div>
                 </div>
             </div>
+        </div> 
+    </div>
+    
+    <div class="section-normal info-panel">
+        <div class="divider-space"></div>
+        <div class="container">
+            <div class="row">
+                <div class="column-md-12">
+                    <div class="row blox-row">
+                        <div class='info-panel-content'>
+                            <div class="blox-column-content info-box" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                <div class="blox-element service-block text-center" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                    <span class='block-icon'><i class="icon-handbag"></i></span>
+                                    <h4>Largest directory of fashion online</h4>
+                                    <p>We've collected everything you need to know about online clothing stores, from sizes and styles to shipping costs and return policies.</p>
+                                </div>
+                            </div>
+                            <div class="blox-column-content info-box">
+                                <div class="service-block text-center" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                    <i class="icon-like"></i>
+                                    <h4>Read reviews from savvy shoppers</h4>
+                                    <p>Get all the inside knowledge about stores you haven't shopped at yet and discover hidden gems.</p>
+                                </div>
+                            </div>
+                            <div class="blox-column-content info-box">
+                                <div class="service-block text-center" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                    <i class="icon-bubbles"></i>
+                                    <h4>Share your experience</h4>
+                                    <p>Tell the world about your favorite stores, from their great customer service to their perfectly tailored cuts.</p>
+                                </div>
+                            </div>
+                            <div class="blox-column-content info-box">
+                                <div class="service-block text-center" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                    <i class="icon-basket"></i>
+                                    <h4>Shop with confidence</h4>
+                                    <p>Don't know what will fit or how fabrics will feel? Shop only at stores that offer free shipping and free returns.</p>
+                                </div>
+                            </div>
+                            <div class="blox-column-content info-box">
+                                <div class="service-block text-center" data-animate="fadeInUp" style="-webkit-animation: 0s;">
+                                    <i class="icon-present"></i>
+                                    <h4>Discover new ways to shop</h4>
+                                    <p>Find the best new sites to shop in different ways, with rentals, subscriptions and personally styled shipments. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    
+    <div class='section-normal categories-panel'>
+        <div class="container">
+            <div class="row">
+                <div class="column-md-12">
+                    <div class="row">
+                        <div class="content">
+                            <h2 class='heading-title'>Categories</h2>
+                            <span class='heading-line'></span>
+                            <div class='categories-panel-content text-center'>
+
+                                <div class="category-content col-sm-12 col-xs-12 col-md-4" id="category-women" style="background-image: 
+                                     url(<?php echo site_url('/wp-content/uploads/Carnaby-Women.png');?> );">
+
+                                    <div class="category-overlay" id="overlay-women">
+                                        <div class="btn btn-secondary category-btn"><a href="<?php echo site_url().'/site_categories/Women';?>">VIEW</a></div>
+                                    </div>
+                                    <div class="category-label" id="women-category-title"><a href="<?php echo site_url().'/site_categories/Women';?>">Women</a></div>
+                                </div>
+                                <div class="category-content col-sm-12 col-xs-12 col-md-4" id="category-men" style="background-image: 
+                                     url(<?php echo site_url('/wp-content/uploads/Carnaby-Men.png');?> );">
+
+                                    <div class="category-overlay" id="overlay-men">
+                                        <div class="btn btn-secondary category-btn"><a href="<?php echo site_url().'/site_categories/Men';?>">VIEW</a></div>
+                                    </div>
+                                    <div class="category-label" id="men-category-title"><a href="<?php echo site_url().'/site_categories/Men';?>">Men</a></div>
+                                </div>
+                                <div class="category-content col-sm-12 col-xs-12 col-md-4" id="category-kids" style="background-image: 
+                                     url(<?php echo site_url('/wp-content/uploads/Carnaby-Kids.png');?> );">
+
+                                    <div class="category-overlay" id="overlay-kids">
+                                        <div class="btn btn-secondary category-btn"><a href="<?php echo site_url().'/site_categories/kids-baby';?>">VIEW</a></div>
+                                    </div>
+                                    <div class="category-label" id="kids-category-title"><a href="<?php echo site_url().'/site_categories/kids-baby';?>">Kids & Babies</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class='section-normal posts-panel'>
+        <div class="container">
+            <div class="row">
+                <div class="column-md-12">
+                    <div class="row">
+                        <div class="content">
+                            <h2 class='heading-title'>Recent Trends & Tips</h2>
+                            <span class='heading-line'></span>
+                            <div class="posts-panel-content text-center">
+                                <?php echo do_shortcode('[do_widget "Carnaby Recent Posts Static"]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </section>
 
 
